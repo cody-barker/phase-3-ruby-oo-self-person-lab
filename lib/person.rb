84bf4c73bb5a8fc4df6da76accea699c596e9009
Cody <codybarker.or@gmail.com>
2023-04-12 11:10:46 -0700

    s

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,15 @@
-# your code goes here
+class Person
+    attr_writer :bank_account, :happiness, :hygiene
+    attr_reader :name, :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(value)
+        @happines =value(0..10)
+    end
+end
